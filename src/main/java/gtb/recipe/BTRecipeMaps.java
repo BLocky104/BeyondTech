68a83bb5f46bdd24d198fc35abc6388db35453b7
BL00DKNIGHT <58563153+BL00DKNIGHT@users.noreply.github.com>
2023-06-30 22:12:06 -0300

    Update and rename GTBRecipeMaps.java to BTRecipeMaps.java

--- src/main/java/gtb/recipe/BTRecipeMaps.java
+++ src/main/java/gtb/recipe/BTRecipeMaps.java
@@ -1,9 +1,9 @@
-package gtb.recipe;
+package bt.recipe;
 
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
 
-public class GTBRecipeMaps {
+public class BTRecipeMaps {
 
     public static final RecipeMap<SimpleRecipeBuilder> TEST_RECIPES = new RecipeMap<>("test", 1, 0, 0, 0, new SimpleRecipeBuilder(), false);
 
